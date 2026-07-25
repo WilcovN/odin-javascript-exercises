5f208417477924b116b8a83a9db66396d76a8cf1
WilcovN <wilco.van.nes@hotmail.com>
2026-07-25 09:43:04 +0200

    Solve reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+// const reverseString = function(str) {
+//     let arr = str.split("");
+//     let reverse = '';
+//     let lastElement;
+//     while (arr.length > 0){
+//         lastElement = arr.pop();
+//         reverse = reverse + lastElement;
+//     }
+//     return reverse;
+// };
 
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
-
+reverseString("hello")
+// console.log("hello".split("").join(""))
 // Do not edit below this line
 module.exports = reverseString;
