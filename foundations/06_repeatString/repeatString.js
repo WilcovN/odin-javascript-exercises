ac59f1df65a830974b9f053a9182f814055cce50
WilcovN <wilco.van.nes@hotmail.com>
2026-07-25 08:58:05 +0200

    Solve repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repeat = '';
+    for (let i = 0; i < num; i++){
+        if (i==0){
+            repeat = str
+        } else {
+            repeat = repeat + str
+        }
+    }
+    return repeat;
 };
 
 // Do not edit below this line
