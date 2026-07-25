79f83007b3fb6b222b06c158cefc6f8cee61c0fb
WilcovN <wilco.van.nes@hotmail.com>
2026-07-25 10:43:27 +0200

    Solve removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr, ...remove) {
+//     let indexRemoved;
+//     for (const item of remove){
+//         while (arr.includes(item)) {
+//             indexRemoved = arr.indexOf(item, 0);
+//             arr.splice(indexRemoved, 1);
+//         }
+//     }
+//     return arr
+// };
+
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter(item => !remove.includes(item));
 };
 
 // Do not edit below this line
