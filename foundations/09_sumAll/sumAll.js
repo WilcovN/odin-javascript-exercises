d249835862d30dcb2f0d370ccf5f1b4247f2e27e
WilcovN <wilco.van.nes@hotmail.com>
2026-07-25 11:38:27 +0200

    Solve sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+// const sumAll = function(begin, end) {
+//     if (begin <= 0 || end <= 0 || !Number.isInteger(begin) || !Number.isInteger(end)) {
+//         return 'ERROR'
+//     }
+//     if (end < begin){
+//         let tempBegin = begin;
+//         begin = end;
+//         end = tempBegin;
+//     }
+//     let sum = end;
+//     for (let i = begin; i < end; i++){
+//         // console.log(i)
+//         // console.log(sum)
+//         sum = sum + i;
+//         // console.log(i)
+//         // console.log(sum)
+//     }
+//     return sum
+// };
 
+const sumAll = function(begin, end) {
+    if (begin <= 0 || end <= 0 || !Number.isInteger(begin) || !Number.isInteger(end)) {
+        return 'ERROR'
+    }
+    if (end < begin){
+        let tempBegin = begin;
+        begin = end;
+        end = tempBegin;
+    }
+    const gaussSum = n => n * (n + 1) / 2;
+    return gaussSum(end) - gaussSum(begin - 1);
 };
 
 // Do not edit below this line
